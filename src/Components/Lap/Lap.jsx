a351a7ac20cc2a2475320e0f46f78264efbb3353
mina ghoreshi <goreishi45@gmail.com>
2023-11-12 00:28:13 +0330

    style: implement lap section style

--- src/Components/Lap/Lap.jsx
+++ src/Components/Lap/Lap.jsx
@@ -1,3 +1,29 @@
 import React from "react";
 import styles from "./Lap.module.css";
-export default function Lap() {}
+export default function Lap() {
+  return (
+    <table className={styles.table}>
+      <thead>
+        <tr>
+          <th>Lap</th>
+          <th>Time</th>
+          {/* Add more columns as needed */}
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>00:06:20</td>
+        </tr>{" "}
+        <tr>
+          <td>1</td>
+          <td>00:06:20</td>
+        </tr>{" "}
+        <tr>
+          <td>1</td>
+          <td>00:06:20</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
